1fd7e08c09ca373d97726088efd59db5f9390dd2
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-02-18 15:13:17 +0300

    Fix run.py

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import os
-from pathlib import Path
 
-os.system("chmod -R 0777 /model")
 
 with open('/model/run.sh', 'w') as f:
 
@@ -11,7 +9,4 @@ with open('/model/run.sh', 'w') as f:
         f.write(f'mkdir /results/{video}\n')
         f.write(f'python3 /model/codes/test.py -opt /model/codes/options/df2k/test_df2k.yml -in_path /dataset/{video} -res_path /results/{video}\n')
 
-    f.write('chmod -R 0777 /results\n')
-
-os.system("chmod -R 0777 /model")
 os.system('/model/run.sh')
