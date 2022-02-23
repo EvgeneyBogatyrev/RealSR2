8b7e2af6d9889f5b6a3cbf2bbf64978879a08d5d
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-02-23 13:42:18 +0300

    Fix permission

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ with open('/model/run.sh', 'w') as f:
         f.write(f'mkdir /results/{video}\n')
         f.write(f'python3 /model/codes/test.py -opt /model/codes/options/df2k/test_df2k.yml -in_path /dataset/{video} -res_path /results/{video}\n')
 
+os.system('chmod +x /model/run.sh')
 os.system('/model/run.sh')
